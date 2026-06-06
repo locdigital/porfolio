bf90fad8899e743080e33b3af12dd71d84a49858
Nguyen Phuc Loc <nguyenphucloc@Thu-nhap-6K.local>
2026-06-06 11:52:18 +0700

    Update font to Imbue and add glassmorphism to nav

---
+++ script/move.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+mv public/assets/photos/photo-1.jpg public/assets/photos/phu-quy/photo-1.jpg
+mv public/assets/photos/photo-2.jpg public/assets/photos/phu-quy/photo-2.jpg
+mv public/assets/photos/photo-3.jpg public/assets/photos/phu-quy/photo-3.jpg
+mv public/assets/photos/photo-4.jpg public/assets/photos/phu-quy/photo-4.jpg
+mv public/assets/photos/photo-5.jpg public/assets/photos/phu-yen/photo-1.jpg
+mv public/assets/photos/photo-6.jpg public/assets/photos/phu-yen/photo-2.jpg
+mv public/assets/photos/photo-7.jpg public/assets/photos/phu-yen/photo-3.jpg
+mv public/assets/photos/photo-8.jpg public/assets/photos/phu-yen/photo-4.jpg
+mv public/assets/photos/photo-9.jpg public/assets/photos/quy-nhon/photo-1.jpg
+mv public/assets/photos/photo-10.jpg public/assets/photos/quy-nhon/photo-2.jpg
+mv public/assets/photos/photo-11.jpg public/assets/photos/quy-nhon/photo-3.jpg
+mv public/assets/photos/photo-12.jpg public/assets/photos/quy-nhon/photo-4.jpg
+mv public/assets/photos/photo-13.jpg public/assets/photos/da-lat/photo-1.jpg
+mv public/assets/photos/photo-14.jpg public/assets/photos/da-lat/photo-2.jpg
+mv public/assets/photos/photo-15.jpg public/assets/photos/da-lat/photo-3.jpg
